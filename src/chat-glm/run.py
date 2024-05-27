2b0fe216244ab320809c134f31685fba1ef52700
linuzb <levizebulon@gmail.com>
2024-05-27 17:14:52 +0800

    rm ls

--- src/chat-glm/run.py
+++ src/chat-glm/run.py
@@ -3,6 +3,7 @@ from transformers import AutoTokenizer, AutoModel
 tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True)
 
 model = AutoModel.from_pretrained("THUDM/chatglm3-6b", trust_remote_code=True).half().cuda()
+
 model = model.eval()
 
 response, history = model.chat(tokenizer, "你好", history=[])
